e8e2f958674af67a247969104fd77fd8f672cce7
mnguyen0226 <mnguyen0226@vt.edu>
2024-01-14 11:22:43 -0800

    add grid

---
+++ javascripts/etch_a_sketch/scripts.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const container = document.getElementById('grid-container');
+    for (let i = 0; i < 16 * 16; i++) {
+        const div = document.createElement('div');
+        div.classList.add('grid-item'); 
+        container.appendChild(div);
+    }
+});
