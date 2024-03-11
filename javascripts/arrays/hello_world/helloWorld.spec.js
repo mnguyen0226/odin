ec19d41c7f3606613b9aeb441d2c7428078a16b2
mnguyen0226 <mnguyen0226@vt.edu>
2024-03-10 19:53:06 -0700

    Reset Javascript

--- javascripts/arrays/hello_world/helloWorld.spec.js
+++
@@ -1,9 +0,0 @@
-// get the result from function
-const helloWorld = require('./helloWorld');
-
-// TDD
-describe('Hello World', function() {
-  test('says "Hello, World!"', function() {
-    expect(helloWorld()).toEqual('Hello, World!');
-  });
-});
--- javascripts/dom/lecture_events_loop/script.js
+++
@@ -1,9 +0,0 @@
-// get all the button 
-const buttons = document.querySelectorAll("button")
-
-// for each button, create alert 
-buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-        alert(button.id);
-    })
-})
